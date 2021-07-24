2adc7c22a6f25e96ed857a411abb3eca214f91eb
jaehyun1009 <j.jaehuyn.1009@gmail.com>
2021-07-23 22:41:32 -0700

    setup profile index page

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -6,15 +6,19 @@ export {
 }
 
 function index(req, res){
-    res.render('profiles/index', {
-        title: "Profile Page",
-        user: req.user
+    Profile.find({}).then(profiles => {
+        res.render('profiles/index', {
+            title: "Users",
+            user: req.user,
+            profiles
+        })
+    }).catch(error => {
+        res.redirect('/')
     })
 }
 
 function show(req, res){
     Profile.findById(req.params.id).then(profile => {
-        console.log(profile)
         res.render('profiles/show', {
             title: `${profile.name}'s Profile`,
             user: req.user,
